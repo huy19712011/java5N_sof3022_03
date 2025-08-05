9864e96e144f30055d236cebda83728ebb9a8805
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-08-05 08:03:15 +0700

    Setup spring boot MVC project, application.properties, pom.xml

---
+++ src/main/java/org/example/java5n_sof3022_03/Java5NSof302203Application.java
@@ -0,0 +1,14 @@
+package org.example.java5n_sof3022_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5NSof302203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java5NSof302203Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/java5n_sof3022_03/Java5NSof302203ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java5n_sof3022_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java5NSof302203ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
