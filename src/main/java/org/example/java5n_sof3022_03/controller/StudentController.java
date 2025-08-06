1b9613bd4056dbbe8559d9e3df879c8c07d22403
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-08-06 09:01:44 +0700

    CRUD 1 table: add a new Student

--- src/main/java/org/example/java5n_sof3022_03/controller/StudentController.java
+++ src/main/java/org/example/java5n_sof3022_03/controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -27,5 +29,24 @@ public class StudentController {
         model.addAttribute("students", students);
         return "views/students";
 
+    }
+
+    @GetMapping("/students/showNewStudentForm")
+    public String showNewStudentForm(Model model) {
+
+        Student student = new Student();
+        model.addAttribute("student", student);
+
+        return "views/new_student";
+    }
+
+    @PostMapping("/students/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+
+        // save student to DB
+        studentService.saveStudent(student);
+
+        // return to view
+        return "redirect:/students";
     }
 }
--- src/main/java/org/example/java5n_sof3022_03/repository/StudentRepository.java
+++ src/main/java/org/example/java5n_sof3022_03/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package org.example.java5n_sof3022_03.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.java5n_sof3022_03.entity.Student;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional
 public class StudentRepository {
 
     private final EntityManager em;
@@ -16,5 +18,10 @@ public class StudentRepository {
     public List<Student> getAllStudents() {
 
         return em.createQuery("FROM Student", Student.class).getResultList();
+    }
+
+    public void saveStudent(Student student) {
+
+        em.persist(student);
     }
 }
